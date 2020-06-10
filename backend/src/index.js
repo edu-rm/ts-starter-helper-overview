ea2ba3ada5eaeb7223ec368e9fe9b9e00552a761
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-10 20:23:13 -0300

    tsconfig and script

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-app.get('/', function (req, res) {
-    return res.send('Hello World');
-});
-app.listen(3333);
